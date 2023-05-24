b925d9fd497a05c53e44086165dcec8c252a5c7c
yichenyang01 <97128432+yichenyang01@users.noreply.github.com>
2023-05-24 16:53:41 -0700

    Update data_insert.py

--- src/indoorbotany/data_insert.py
+++ src/indoorbotany/data_insert.py
@@ -1,36 +1,40 @@
 import mysql.connector as mysql
 
 if __name__ == '__main__':
-    db = mysql.connect(host="sql9.freemysqlhosting.net", user="sql9614741", password="gNXJLalcwM", database="sql9614741")
+    db = mysql.connect(host="sql9.freesqldatabase.com", user="sql9617434", password="x4E41uqNky", database="sql9617434")
 
     cursor = db.cursor()
     print('Connected')
+    cursor.execute("""CREATE TABLE quiz (
+      id INT AUTO_INCREMENT PRIMARY KEY,
+      question VARCHAR(255) NOT NULL,
+      option1 VARCHAR(255) NOT NULL,
+      option2 VARCHAR(255) NOT NULL,
+      option3 VARCHAR(255) NOT NULL,
+      correct_answer VARCHAR(255) NOT NULL
+    );""")
 
-    # questions = [
-    #     ("What is the ideal temperature range for most indoor plants?",
-    #      "40-50°F (4-10°C)", "60-70°F (15-21°C)", "80-90°F (27-32°C)", "B"),
-    #     ("What type of soil is best for indoor plants?",
-    #      "Sand", "Clay", "Loamy", "C"),
-    #     ("What is the most important factor for successful indoor plant growth?",
-    #      "Sunlight", "Watering", "Fertilizer", "A"),
-    #     ("Which of the following is a common indoor plant pest?",
-    #      "Grasshopper", "Spider mite", "Caterpillar", "B"),
-    #     ("Which of the following is an effective way to increase humidity for indoor plants?",
-    #      "Watering more frequently", "Using a humidifier", "Opening windows", "B"),
-    #     ("Which of the following is NOT a common method of propagating plants at home?",
-    #      "Stem cuttings", "Layering", "Shaking", "C"),
-    #     ("What is the process of acclimating plants to new growing conditions called?",
-    #      "Adaptation", "Transplanting", "Hardening off", "C"),
-    #     ("What is the name of the process by which plants convert sunlight into energy?",
-    #      "Photosynthesis", "Respiration", "Transpiration", "A"),
-    #     ("What is the recommended pH range for most indoor plants?",
-    #      "1-3", "4-6", "7-9", "B"),
-    #     ("What is the best time of day to water indoor plants?",
-    #      "Morning", "Afternoon", "Evening", "A")
-    #
-    # ]
-    #
     questions = [
+        ("What is the ideal temperature range for most indoor plants?",
+         "40-50°F (4-10°C)", "60-70°F (15-21°C)", "80-90°F (27-32°C)", "B"),
+        ("What type of soil is best for indoor plants?",
+         "Sand", "Clay", "Loamy", "C"),
+        ("What is the most important factor for successful indoor plant growth?",
+         "Sunlight", "Watering", "Fertilizer", "A"),
+        ("Which of the following is a common indoor plant pest?",
+         "Grasshopper", "Spider mite", "Caterpillar", "B"),
+        ("Which of the following is an effective way to increase humidity for indoor plants?",
+         "Watering more frequently", "Using a humidifier", "Opening windows", "B"),
+        ("Which of the following is NOT a common method of propagating plants at home?",
+         "Stem cuttings", "Layering", "Shaking", "C"),
+        ("What is the process of acclimating plants to new growing conditions called?",
+         "Adaptation", "Transplanting", "Hardening off", "C"),
+        ("What is the name of the process by which plants convert sunlight into energy?",
+         "Photosynthesis", "Respiration", "Transpiration", "A"),
+        ("What is the recommended pH range for most indoor plants?",
+         "1-3", "4-6", "7-9", "B"),
+        ("What is the best time of day to water indoor plants?",
+         "Morning", "Afternoon", "Evening", "A"),
         ('What is the optimal humidity range for most indoor plants?',
          '20-30%', '40-60%', '70-80%', 'B'),
         ('Which of the following is NOT a common nutrient deficiency in plants?',
