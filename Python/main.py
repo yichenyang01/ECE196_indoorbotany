bec5b2e262add1e2dbe89a9c996476c3de2699a6
yichenyang01 <97128432+yichenyang01@users.noreply.github.com>
2023-06-01 17:37:43 -0700

    Update main.py

--- Python/main.py
+++ Python/main.py
@@ -10,8 +10,11 @@ db_user = os.environ['MYSQL_USER']
 db_pass = os.environ['MYSQL_PASSWORD']
 db_name = os.environ['MYSQL_DATABASE']'''
 
-db = mysql.connect(host="sql3.freesqldatabase.com", user="sql3493972", password="CB6YQQVruM",
-                   database="sql3493972")
+db = mysql.connect(host="sql9.freesqldatabase.com",
+                   user="sql9622826",
+                   password="hQNVN4TgY4",
+                   database="sql9622826")
+
 cursor = db.cursor()
 
 url = "http://192.168.188.175/"  # http://192.168.1.212/   http://192.168.188.251/
@@ -25,20 +28,19 @@ try:
         # print(soup.prettify())
 
         data = []
-        for div in soup.find_all("div"):
-            for p in div.find_all("p"):
-                data.append(float(p.text))
+        for p in soup.find_all("p"):
+            data.append(float(p.text))
 
         print(data)
 
         # time.sleep(0.02)
 
-        query = "INSERT INTO sensorData (temperature, moisture, humidity, sunlight) VALUES " \
-                "({}, {}, {}, {}, {}, {}, {})".format(data[0], data[1], data[2], data[3], data[4], data[5], data[6])
-        print(query)
-
-        cursor.execute(query)
-        db.commit()
+        # query = "INSERT INTO sensorData (temperature, moisture, humidity, sunlight) VALUES " \
+        #         "({}, {}, {}, {}, {}, {}, {})".format(data[0], data[1], data[2], data[3], data[4], data[5], data[6])
+        # print(query)
+        #
+        # cursor.execute(query)
+        # db.commit()
 
 except:
     pass
